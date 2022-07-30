534b38095f624c5f84949a1f38cc407a24d3b8bb
Askar Sabiq <askarsabiq@protonmail.ch>
2022-07-30 16:58:47 +0700

    Done!

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -1,43 +1,63 @@
-const canvas = document.getElementById('etch-a-sketch');
+
+const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakeButton = document.querySelector('.shake');
-const MOVE_AMOUNT = 10;
+const MOVE_AMOUNT = 50;
 
 // setup canvas
-const {width, height} = canvas;
-
-console.log(`Width is ${width}, height is ${height}`);
+const { width, height } = canvas;
 
 let x = Math.floor(Math.random() * width);
 let y = Math.floor(Math.random() * height);
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 100;
+ctx.lineWidth = MOVE_AMOUNT;
 
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 ctx.beginPath();
-ctx.moveTo = (x, y);
-ctx.lineTo = (x, y);
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
 
 function draw({ key }) {
+    hue += 1;
+    console.log(hue);
+    ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
     console.log(key);
     ctx.beginPath();
-    ctx.moveTo = (x, y)
-    x = x - 10;
-    y = y - 10;
-    ctx.lineTo = (x, y);
+    ctx.moveTo(x, y);
+    switch (key) {
+        case 'ArrowUp':
+            y -= MOVE_AMOUNT;
+            break;
+        case 'ArrowRight':
+            x += MOVE_AMOUNT;
+            break;
+        case 'ArrowDown':
+            y += MOVE_AMOUNT;
+            break;
+        case 'ArrowLeft':
+            x -= MOVE_AMOUNT;
+            break;
+        default:
+    }
+    ctx.lineTo(x, y);
     ctx.stroke();
-}
+};
 
 function handlekey(e) {
     if (e.key.includes('Arrow')) {
         e.preventDefault();
-        draw({
-            key: e.key
-        });
-        console.log('Handling key...');
+        draw({key: e.key});
     }
+};
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, width, height);
+    canvas.classList.toggle('shake');
 }
 
 window.addEventListener('keydown', handlekey);
+shakeButton.addEventListener('click', clearCanvas);
